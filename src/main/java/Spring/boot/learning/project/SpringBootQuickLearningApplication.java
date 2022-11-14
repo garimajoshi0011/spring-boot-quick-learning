6b75cea7b1448c0c50d7c9ab352a115a33759176
Rahul <pantrahul02@gmail.com>
2022-11-14 18:04:18 +0530

    initial commit

---
+++ src/main/java/Spring/boot/learning/project/SpringBootQuickLearningApplication.java
@@ -0,0 +1,13 @@
+package Spring.boot.learning.project;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootQuickLearningApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootQuickLearningApplication.class, args);
+	}
+
+}
---
+++ src/test/java/Spring/boot/learning/project/SpringBootQuickLearningApplicationTests.java
@@ -0,0 +1,13 @@
+package Spring.boot.learning.project;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootQuickLearningApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
